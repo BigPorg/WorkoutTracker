0e70c2833519c4caeb3e7a9869d606be430c2e72
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-07-29 17:08:58 -0400

    testing .'s

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -7,16 +7,16 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 // missed stats!
 router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/stats.html"));
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 module.exports = router;
