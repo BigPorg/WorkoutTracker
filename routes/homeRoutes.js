e7c4e0bb290ed963134a4fe84759fbb5688ef2fe
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-07-29 12:41:55 -0400

    home routes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -3,3 +3,15 @@
 // router and path npm packages
 // __dirname
 
+const path = require("path");
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+module.exports = router;
